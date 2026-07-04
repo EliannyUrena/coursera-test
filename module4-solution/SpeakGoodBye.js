9c8f9d97fc7aba62756744212304067bf5e998a9
Elianny Urena <eliannyurenafdez17@gmail.com>
2026-07-03 21:52:18 -0400

    Agregando asignacion modulo 5

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -5,7 +5,6 @@
 // See Lecture 52, part 2
 (function () {
 
-
   // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
   // the "speak" method and which you will expose to the global context
   // See Lecture 52, part 1
@@ -24,7 +23,5 @@
   // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
   // 'byeSpeaker' on the global scope as well.
   window.byeSpeaker = byeSpeaker;
-
-
 
 })();
